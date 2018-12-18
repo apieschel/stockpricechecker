772333e806a5edfef0ac1028cf52a23df4d324be
Glitch (chisel-grade) <none>
2018-12-18 18:00:42 +0000

    🚟🌻 Checkpoint ./routes/api.js:2265073/628 ./package.json:2265073/34

--- routes/api.js
+++ routes/api.js
@@ -8,8 +8,9 @@
 
 'use strict';
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
+const expect = require('chai').expect;
+const MongoClient = require('mongodb');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
@@ -19,18 +20,24 @@ module.exports = function (app) {
     .get(function (req, res){
       const xhr = new XMLHttpRequest();   
       const callback = function(err, data) {
+        if(err) {
+          res.json(err);
+        } else {
+          res.json("Hello, Alex");
+        }
       }
-      xhr.open('GET', 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=demo', true);
+      xhr.open('GET', 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json', true);
       xhr.responseType = 'json';
       xhr.onload = function() {
         var status = xhr.status;
+        console.log(status);
         if (status === 200) {
+          console.log(xhr.response);
           callback(null, xhr.response);
         } else {
           callback(status, xhr.response);
         }
       };
       xhr.send();
-      res.json("Hello, Alex");  
     });
 };
