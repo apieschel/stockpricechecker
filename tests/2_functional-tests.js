3770f6ad8838bd9589648137b24749c1eb193219
Glitch (chisel-grade) <none>
2018-12-19 23:24:47 +0000

    🚇🌓 Checkpoint ./tests/2_functional-tests.js:2265073/493

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -16,34 +16,38 @@ chai.use(chaiHttp);
 suite('Functional Tests', function() {
     
     suite('GET /api/stock-prices => stockData object', function() {
-      
+      /*
       test('1 stock', function(done) {
        chai.request(server)
         .get('/api/stock-prices')
         .query({stock: 'goog'})
         .end(function(err, res){
           assert.equal(res.type, 'application/json', 'check if the response is json');
-          assert.equal(typeof res.body.price, 'string', 'check for the price string is there');
+          assert.equal(typeof res.body.price, 'string', 'check that the price string is there');
           assert.equal(res.status, 200, 'response status should be 200');
-          assert.equal(res.body.ticker, 'GOOG', 'check if the stock name is returned'); 
+          assert.equal(res.body.ticker, 'GOOG', 'check if the correct stock name is returned'); 
           done();
         });
       });
+      */
       
       test('1 stock with like', function(done) {
         chai.request(server)
         .get('/api/stock-prices')
-        .query({stock: 'goog', like: 'true'})
+        .query({stock: 'aapl', like: 'true'})
         .end(function(err, res){
           console.log(res.body);
           assert.equal(res.status, 200, 'response status should be 200');
           assert.equal(res.type, 'application/json', 'check if the response is json');
-          assert.equal(typeof res.body.price, 'string', 'check for the price string is there');
-          assert.equal(res.body.ticker, 'GOOG', 'check if the stock name is returned'); 
+          assert.equal(typeof res.body.price, 'string', 'check that the price string is there');
+          assert.equal(typeof res.body.likes, 'number', 'check that the number of likes exists');
+          assert.equal(typeof res.body.ips, 'object', 'check for the array of ip addresses');
+          assert.equal(res.body.ticker, 'AAPL', 'check if the correct stock name is returned'); 
           done();
         });  
       });
       
+      /*
       test('1 stock with like again (ensure likes arent double counted)', function(done) {
         chai.request(server)
         .get('/api/stock-prices')
@@ -82,6 +86,7 @@ suite('Functional Tests', function() {
           done();
         });
       });
+      */
       
     });
 
