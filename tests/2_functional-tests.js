b96aba94b19292264033d365d7941b24614237e1
Glitch (chisel-grade) <none>
2018-12-19 23:35:04 +0000

    💎🐦 Checkpoint ./tests/2_functional-tests.js:2265073/630

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -22,14 +22,14 @@ suite('Functional Tests', function() {
         .get('/api/stock-prices')
         .query({stock: 'goog'})
         .end(function(err, res){
+          assert.equal(res.status, 200, 'response status should be 200');
           assert.equal(res.type, 'application/json', 'check if the response is json');
           assert.equal(typeof res.body.price, 'string', 'check that the price string is there');
-          assert.equal(res.status, 200, 'response status should be 200');
           assert.equal(res.body.ticker, 'GOOG', 'check if the correct stock name is returned'); 
           done();
         });
       });
-      */
+      
       
       test('1 stock with like', function(done) {
         chai.request(server)
@@ -47,20 +47,21 @@ suite('Functional Tests', function() {
         });  
       });
       
-      /*
+      
       test('1 stock with like again (ensure likes arent double counted)', function(done) {
         chai.request(server)
         .get('/api/stock-prices')
-        .query({stock: 'goog'})
+        .query({stock: 'aapl', like: 'true'})
         .end(function(err, res){
-          assert.equal(res.type, 'application/json', 'check if the response is json');
-          assert.equal(typeof res.body.price, 'string', 'check for the price string is there');
           assert.equal(res.status, 200, 'response status should be 200');
-          assert.equal(res.body.ticker, 'GOOG', 'check if the stock name is returned'); 
+          assert.equal(res.type, 'application/json', 'check if the response is json');
+          assert.equal(res.body, 'You have already liked this stock!', 'check for the correct notification message'); 
           done();
         });
       });
+      */
       
+      /*
       test('2 stocks', function(done) {
         chai.request(server)
         .get('/api/stock-prices')
