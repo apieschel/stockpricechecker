28a67d3d2615d4519788915ceede5593ad2841e8
Glitch (chisel-grade) <none>
2019-02-06 05:35:13 +0000

    📕📖 Checkpoint ./tests/1_unit-tests.js:2265073/205

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,20 +1,7 @@
-/*
-*
-*
-*       FILL IN EACH UNIT TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]----
-*       (if additional are added, keep them at the very end!)
-
-
-var chai = require('chai');
-var StockHandler = require('../controllers/stockHandler.js');
-
-var stockPrices = new StockHandler();
+const chai = require('chai');
+const StockHandler = require('../controllers/stockHandler.js');
+const stockPrices = new StockHandler();
 
 suite('Unit Tests', function(){
-
-// none for now
-
+  // none for now
 });
-
-*/
